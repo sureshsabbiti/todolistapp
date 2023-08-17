526840735710299478cc0430432274760906d29e
sureshsabbiti <sabbitisuresh3@gmail.com>
2023-08-17 22:20:22 +0530

    todolist

---
+++ TodoApp.js
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+import TodoList from './TodoList';
+import "./index.css";
+
+const TodoApp = () => {
+  const [task,setTask] = useState("");
+  const [todos,setTodos] = useState([]);
+
+  const changeHandler = e =>{
+    setTask(e.target.value)
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    const newTodos = [...todos,task];
+    setTodos(newTodos);
+    setTask("");
+  }
+  const deleteHandler = (indexValue) =>{
+    const newTodos = todos.filter((_,index) => index !== indexValue);
+    setTodos(newTodos);
+  }
+  return (
+    <div>
+      <center>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Todo Management Application</h5>
+            <form onSubmit={submitHandler}>
+              <input size="30" type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
+              <input type="button" value="Add" name="Add"/>
+            </form>
+            <TodoList todolist={todos} deleteHandler={deleteHandler}/>
+          </div>
+        </div>
+      </center>
+    </div>
+  )
+}
+
+export default TodoApp
